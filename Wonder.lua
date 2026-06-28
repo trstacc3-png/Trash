3976169c88ef3ec4cfc667682846d630a0ec34e7
trstacc3-png <trstacc3@gmail.com>
2026-06-28 23:43:53 +0700

    Update Wonder.lua

--- Wonder.lua
+++ Wonder.lua
@@ -143,7 +143,7 @@ logConn = LogService.MessageOut:Connect(function(message, type)
     end
 end)
 
-local url = "https://raw.githubusercontent.com/trstacc3-png/Trash/refs/heads/main/words.txt"
+local url = "https://raw.githubusercontent.com/trstacc3-png/Trash/refs/heads/main/vivi-result.txt"
 local fileName = "ultimate_words_v5.txt"
 
 -- Temporary Loading UI
